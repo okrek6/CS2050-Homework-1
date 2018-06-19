2d663efa663519a6f23da0170e836f05a5ea334d
Brendan Krekeler <krekeler@brendans-mbp.lan>
2018-06-19 00:35:52 -0500

    Added more to the Homework assignment

--- hmwrk3/main.c
+++ hmwrk3/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 typedef struct player{
     char Fname[25];
@@ -22,12 +23,23 @@ typedef struct player{
 
 int main(int argc, const char * argv[])
 {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    
+    int numOfPlayers = atoi(argv[1]);
+    
+    
+    
+    
+    
+    //malloc(sizeof(Player) * numOfPlayers);
+    
+//    printf("%s", readFromFile(argv[2], struct Player, 1, numOfPlayers));
+    
+    
+    
+    
 }
 
-void readFromFile(char* filename, Player* players, int index, int size)
+void readFromFile(char* filename, Player* players, int i, int size)
 {
     
     FILE *file = fopen(filename, "r");
@@ -36,20 +48,14 @@ void readFromFile(char* filename, Player* players, int index, int size)
     {
         printf("Could not read file");
     }
-    
-    Player *playersArray = malloc(sizeof(Player));
-    
-    for (index = 0; index < size; index++) {
-        Player temp;
+
+    for (i = 0; i < size; i++) {
         
-        
-        
+        fscanf(file, "%s %s %d %d %d %d %d %f", &players[i].Fname[25], &players[i].Lname[25], &players[i].Singles, &players[i].Doubles,  &players[i].Triples, &players[i].Homeruns, &players[i].At_Bats, &players[i].Slugging_Percentage);
         
     }
     
-    
-    
-    
+    // make sure you look at the array location at each file, becasue you could overwrite sectrions of the array if you are not careful.
     // This function will read in size struct players from filename and add these
     // the players array. The function will use index to know where to start
     // writing the players to in the array.
@@ -64,18 +70,48 @@ void readFromFile(char* filename, Player* players, int index, int size)
 
 void calculate_slugging(Player* players, int size)
 {
+    int singles, doubles, triples, homeruns;
+    
+    singles = players->Singles;
+    doubles = players->Doubles * 2;
+    triples = players->Triples * 3;
+    homeruns = players->Homeruns * 4;
+    
+    players->Slugging_Percentage = singles + doubles + triples + homeruns / players->At_Bats;
+    
+    
     // This function will take in an array of players and calculate their slugging
     // percentage using the other variables in the structure(Singles, Doubles,
     // Triples, Homeruns).
     // Parameters
     // players – a pointer to the array of Players structures
     // size – the size of the array of structures
+    
+    //look at typecasting under the slugging paragraph for
 
 }
 
 
 void sort_array(Player* players, int size)
 {
+    int i;
+    int j;
+
+    for (i = 0; i < size - 1; i++) {
+        for (j = 0; j < size - i -1; j++) {
+            if( (players+j)->Slugging_Percentage > (players+j)->Slugging_Percentage){
+                Player temp;
+                
+                temp.Slugging_Percentage = players[j].Slugging_Percentage;
+                players[j].Slugging_Percentage = players[j+1].Slugging_Percentage;
+                players[j+1].Slugging_Percentage = temp.Slugging_Percentage;
+
+            }
+        }
+    }
+    
+    //you can use the bubble sort that you used from lab, just make sure that it works correctly
+    
 //This function takes in an array of players and will sort this array based on Slugging      // Percentage. The formula for calculating slugging percentage can be found toward the       // bottom of the document.
 // Parameters
 //
@@ -86,6 +122,9 @@ void sort_array(Player* players, int size)
 
 
 void write_to_file(char* filename, Player* players, int size){
+    
+    //write the sorted array DECENDING ORDER
+    
 // This function will take in a structure of players and print them into the      // given
 // output file, filename
 // Parameters
